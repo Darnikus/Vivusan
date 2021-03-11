41a8ab49745afca63340cf8516539e91d8f23a4f
Darnikus <SuperDarnikus@gmail.com>
2021-03-11 19:07:41 +0200

    Добавлен постгрес, работает на локальной базе данных. Вроде работает все нормально

--- spring/src/main/java/com/vivusan/spring/domain/Food.java
+++ spring/src/main/java/com/vivusan/spring/domain/Food.java
@@ -3,13 +3,11 @@ package com.vivusan.spring.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "foods")
 @AllArgsConstructor
 public class Food {
 
